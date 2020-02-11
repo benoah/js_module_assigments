c50f700dafa6e2b9f71e52f13a56e3c08bce270b
Ben Noah <noahbenaas@gmail.com>
2020-02-11 12:25:27 +0100

    Update script.js

--- week_4/exercise_1/js/script.js
+++ week_4/exercise_1/js/script.js
@@ -1,12 +1,45 @@
-const firstName = document.querySelector("#firstName");
+/*console.log("hello world");
 
-firstName.addEventListener("keyup", logLength);
+const greeting= "hello there people";
+const lengthOfGreeting = greeting.length;
 
-function logLength(event) {
+console.log("string chars", lengthOfGreeting);
+
+const emptyString = "";
+console.log("empty string", emptyString.length);
+
+
+const spaceText= "ab";
+console.log("spaceTest", spaceText.length);*/
+
+const firstName = document.querySelector("#firstNmae");
+firstName. addEventListener("keyup", loglength)
+
+
+function loglength() {
   const inputValue = event.target.value;
   const valueLength = inputValue.length;
-  console.log(valueLength);
+  // console.log(valueLength);
+  // you can aslo write it like this
+  // console.log(valueLength > 0);
+  console.log(inputValue.length);
 
-  // we could skip the variables and write it this way
-  console.log(event.target.value.length);
+  if (valueLength > 0) {
+    console.log("the input has a value");
+  } else {
+    console.log("the input does not have a value");
+  }
 }
+
+// Dette er for å bryte lengden for å få kortere setning(rett og slett dekke space)
+/*
+const firstName = "manny";
+console.log(firstName.length);
+
+const trimedName = firstName.trim();
+console.log(trimedName.length);
+
+const space = " ";
+console.log("how many spaces:"; space.length);
+console.log("remove space" space.trim().length);
+*/
